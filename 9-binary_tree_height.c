33d74efe098195259868ca932cd133a556897ca9
Benjamin <benjamin.alazet@gmail.com>
2023-05-15 15:41:58 +0200

    add functions that measures depth and height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree : pointer to the root node of the tree to traverse
+ * Return: void
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t Left_Height;
+	size_t Right_Height;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	Left_Height = binary_tree_height(tree->left);
+	Right_Height = binary_tree_height(tree->right);
+
+	if (Left_Height > Right_Height)
+		return (Left_Height + 1);
+	else
+		return (Right_Height + 1);
+
+	return (0);
+}
