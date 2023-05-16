294946f5a1f90da8db87bc43ede02c6b5670f355
Malaldiallo93 <mohamedmalal93@gmail.com>
2023-05-16 12:58:05 +0200

    modified betty coding style

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,26 +1,26 @@
 #include "binary_trees.h"
 
 /*
- * binary_tree_size - Measures the size of a binary tree
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree : pointer to the root node of the tree
  * Return: Size of the binary tree,
  */
-size_t binary_tree_nodes(const binary_tree_t *tree) 
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    if (tree == NULL) 
-    {
-        return (0);
-    }
+	if (tree == NULL) 
+	{
+		return (0);
+	}
 
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        count++;
-    }
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		count++;
+	}
 
-    count += binary_tree_nodes(tree->left);
-    count += binary_tree_nodes(tree->right);
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
 
-    return (count);
+	return (count);
 }
